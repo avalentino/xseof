4e3a84c4fb597f2c9b7ee4f5b904f8e8c8c0e7ab
Antonio Valentino <antonio.valentino@tiscali.it>
2022-11-07 00:37:07 +0100

    Parse and from_string functions for all object types

--- xseof/aux_orbdor/__init__.py
+++ xseof/aux_orbdor/__init__.py
@@ -1,1 +1,43 @@
-# nothing here
+"""Doris Precise Orbit File."""
+
+from typing import Union 
+from xsdata.exceptions import ParserError
+from xsdata.formats.dataclass.parsers import XmlParser
+
+
+_type_name = "DorisPreciseFileType"
+_type_description = __doc__.rstrip(".")
+
+
+def parse(source):
+    from . import v01xx, v02xx, v03xx
+
+    parser = XmlParser()
+
+    for pkg in v03xx, v02xx, v01xx:
+        try:
+            return parser.parse(source, getattr(pkg, _type_name))
+        except ParserError:
+            pass
+    else:
+        raise ParserError(
+            f"Unable to load DORIS {_type_description} form {source}")
+
+
+def from_string(source: Union[str, bytes]):
+    from . import v01xx, v02xx, v03xx
+
+    parser = XmlParser()
+
+    if isinstance(source, str):
+        parse = parser.from_string
+    else:
+        parse = parser.from_bytes
+
+    for pkg in v03xx, v02xx, v01xx:
+        try:
+            return parse(source, getattr(pkg, _type_name))
+        except ParserError:
+            pass
+    else:
+        raise ParserError(f"Unable to load {_type_description}")
