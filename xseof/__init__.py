341a864a3eb2ff4f15ae98b34de6e96299cd7e9c
Antonio Valentino <antonio.valentino@tiscali.it>
2022-11-07 07:29:12 +0100

    Add common parse functions to the the top-level package

--- xseof/__init__.py
+++ xseof/__init__.py
@@ -1,1 +1,67 @@
-# nothing here
+"""I/O library for the ESA EOF files based on xsData.
+
+This package provides a set of "dataclasses", comapible with the xsData [1]
+Python library, to access and make I/O operation on the XML files in the
+ESA Earth Observation Ground Segment File Format (EOF) [2].
+
+In particular, this package support all the XML based orbit and attitude
+products described in [2].
+
+[1] https://github.com/tefra/xsdata
+[2] https://eop-cfi.esa.int/Repo/PUBLIC/DOCUMENTATION/SYSTEM_SUPPORT_DOCS/PE-TN-ESA-GS-0001%20EO%20GS%20File%20Format%20Standard%203.0%20signed.pdf
+"""
+
+
+from typing import Union 
+from xsdata.formats.dataclass.parsers import XmlParser
+
+
+__version__ = "1.0.0.dev0"
+
+
+def _import_all():
+    """Lazy import modules for all the products types and all versions."""
+    import xseof.aux_orbdop.v01xx
+    import xseof.aux_orbdop.v02xx
+    import xseof.aux_orbdop.v03xx
+
+    import xseof.aux_orbdor.v01xx
+    import xseof.aux_orbdor.v02xx
+    import xseof.aux_orbdor.v03xx
+
+    import xseof.aux_orbres.v01xx
+    import xseof.aux_orbres.v02xx
+    import xseof.aux_orbres.v03xx
+
+    import xseof.int_attref.v01xx
+    import xseof.int_attref.v02xx
+    import xseof.int_attref.v03xx
+
+    import xseof.mpl_orbpre.v01xx
+    import xseof.mpl_orbpre.v02xx
+    import xseof.mpl_orbpre.v03xx
+
+    import xseof.mpl_orbref.v01xx
+    import xseof.mpl_orbref.v02xx
+    import xseof.mpl_orbref.v03xx
+
+
+def parse(source):
+    _import_all()
+
+    parser = XmlParser()
+
+    return parser.parse(source)
+
+
+def from_string(source: Union[str, bytes]):
+    _import_all()
+
+    parser = XmlParser()
+
+    if isinstance(source, str):
+        parse = parser.from_string
+    else:
+        parse = parser.from_bytes
+
+    return parse(source)
