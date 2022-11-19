eb80985d662b852f47626f9edc1780386d3e91c4
Antonio Valentino <antonio.valentino@tiscali.it>
2022-11-19 22:29:41 +0100

    Typo

--- tools/generate.sh
+++ tools/generate.sh
@@ -6,7 +6,7 @@ TOOLSDIR=$(dirname $0)
 
 sh ${TOOLSDIR}/generate-aux-orbdop.sh
 sh ${TOOLSDIR}/generate-aux-orbdor.sh
-sh ${TOOLSDIR}/generate-aux-orbred.sh
+sh ${TOOLSDIR}/generate-aux-orbres.sh
 sh ${TOOLSDIR}/generate-int-attref.sh
 sh ${TOOLSDIR}/generate-mpl-orbpre.sh
 sh ${TOOLSDIR}/generate-mpl-orbref.sh
